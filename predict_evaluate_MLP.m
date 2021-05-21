e4600efd5c39245d68e4799e4e3fed54adfb2852
Yair Lahad <80884092+Yair-lahad@users.noreply.github.com>
2021-05-21 10:43:33 +0300

    Delete predict_evaluate_MLP.m

--- predict_evaluate_MLP.m
+++
@@ -1,9 +0,0 @@
-function [err, acc] = predict_evaluate_MLP(Net, X, Y, labels)
-%PREDICT_EVALUATE_MLP Predicts then evaluates a MLP
-
-% Get metrics for the given dataset
-Y_out       = predict_MLP(Net, X);
-[err, acc]  = evaluate_MLP(Y_out, Y, labels);
-
-end
-
