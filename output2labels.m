2ceb2061c0dc19428fcd862c1651699f341a88da
Yair Lahad <80884092+Yair-lahad@users.noreply.github.com>
2021-05-21 10:43:51 +0300

    Delete output2labels.m

--- output2labels.m
+++
@@ -1,8 +0,0 @@
-function [labels] = output2labels(Y)
-%OUTPUT2LABELS Convert a MLP's classification output to MNIST labels
-
-[~, labels]	= max(Y);       % Get the index of the neuron with the maximal activity
-labels      = labels' - 1;	% Correct for matlab indices (start with 1)
-
-end
-
